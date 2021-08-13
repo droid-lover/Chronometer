2759e588781ffbcf100f51d6a4ba009bbf6a3dc6
droid-lover <ersachin20@gmail.com>
2021-08-13 14:21:25 +0530

    Added code for starting and stopping the Chronometer

--- app/src/main/java/com/vs/chronometer/MainActivity.kt
+++ app/src/main/java/com/vs/chronometer/MainActivity.kt
@@ -2,14 +2,36 @@ package com.vs.chronometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.vs.chronometer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupChronometer()
     }
+
+    private fun setupChronometer() {
+        binding.chronometer.apply {
+            base = SystemClock.elapsedRealtime()
+        }
+    }
+
+    fun startTimer(view: View) {
+        setupChronometer()
+        binding.chronometer.start()
+    }
+
+    fun stopTimer(){
+        binding.chronometer.stop()
+        Toast.makeText(this,"Timer stopped!",Toast.LENGTH_SHORT).show()
+    }
+
 }
